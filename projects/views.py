6e9d07562f88cc14a4b3349f28c3f1b97c759905
Scott <scott.gordon.dev@outlook.com>
2020-09-05 10:27:00 -0400

    Init commit

---
+++ projects/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from projects.models import Project
+
+# Create your views here.
+
+
+def project_list(request):
+    return render(request, 'projects/index.html')
+
+
+def all_projects(request):
+    # query the db to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', 
+                            {'projects': projects})
